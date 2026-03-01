db8fad2947730d4abdca56ab88fc0816d52a122f
mehmetdemirhan <143554453+mehmetdemirhan@users.noreply.github.com>
2026-03-01 15:47:37 +0300

    Create server.py

---
+++ appbackend/server.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, APIRouter, HTTPException, Depends, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from dotenv import load_dotenv
+from starlette.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+import logging
+from pathlib import Path
+from pydantic import BaseModel, Field, EmailStr
+from typing import List, Optional
+import uuid
+from datetime import datetime, timezone, timedelta
+from enum import Enum
+from passlib.context import CryptContext
+from jose import JWTError, jwt
